440f38a0ae2c56dbf7c3025624442e94bf2f1e46
Дмитрий Кузькин <dimakuzkin119@gmail.com>
2023-09-25 15:03:47 +0300

    Вторая домашка

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+Console.WriteLine("введи номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 6){
+    Console.WriteLine("нет");
+}
+else if (number == 6 || number == 7){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("вообще бред написал");
+}
